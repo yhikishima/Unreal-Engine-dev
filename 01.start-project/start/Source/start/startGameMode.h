85a7a463aa4d440289189f93becba5e7e5cfde3e
yhikishima <lallapalooza1120@gmail.com>
2016-03-03 23:33:03 +0900

    second project commit

---
+++ 01.start-project/start/Source/start/startGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "startGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class START_API AstartGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
